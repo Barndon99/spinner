249d4014b9dbdeacad55b5b93b9a77c8f327d7b7
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-01 10:12:10 -0600

    completed spinner

---
+++ spinner1.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+  process.stdout.write('\r|     ')
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/     ')
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-     ')
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\     ')
+}, 800);
+
+setTimeout(() => {
+  process.stdout.write('\r|     ')
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r/     ')
+}, 1400);
+
+setTimeout(() => {
+  process.stdout.write('\r-     ')
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r\\     ')
+}, 2000);
+
+setTimeout(() => {
+  process.stdout.write('\r|     ')
+}, 2300);
